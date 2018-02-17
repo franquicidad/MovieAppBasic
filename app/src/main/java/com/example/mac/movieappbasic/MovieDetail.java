954b33cd768f90e041bc971d33fed6dba54c3714
mac <franquicidad@gmail.com>
2018-02-17 13:42:33 -0500

    changes 7

--- app/src/main/java/com/example/mac/movieappbasic/MovieDetail.java
+++ app/src/main/java/com/example/mac/movieappbasic/MovieDetail.java
@@ -8,6 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.mac.movieappbasic.Model.Movie;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by mac on 8/02/18.
@@ -28,13 +29,22 @@ public class MovieDetail extends AppCompatActivity{
 
         Intent intent=getIntent();
 
+        Movie movie=getIntent().getExtras().getParcelable("movie");
+
         String movieName=intent.getParcelableExtra("movieName");
         String poster=intent.getParcelableExtra("poster_path");
         String voteAverage=intent.getParcelableExtra("voteAverage");
         String overview=intent.getParcelableExtra("overview");
         String releaseDate=intent.getParcelableExtra("releaseDate");
+        String posterPath=movie.getPoster_path();
 
-        imageDetail =findViewById(R.id.text_movie_name);
-        imageDetail.setTe
+
+        imageDetail=findViewById(R.id.imageMovie_detail_activity);
+
+        Picasso.with(this).load(posterPath).into(imageDetail);
+
+        titleDetail =findViewById(R.id.text_movie_name);
+        titleDetail.setText(movieName);
+
     }
 }
